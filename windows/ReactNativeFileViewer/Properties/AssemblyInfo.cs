c7c2b6d1a04c0a09587a90cc72f6f609a2a587d3
Matteo Pagani <matteo.pagani@microsoft.com>
2021-03-29 17:37:46 +0200

    Migrated module to 0.64

--- windows/ReactNativeFileViewer/Properties/AssemblyInfo.cs
+++ windows/ReactNativeFileViewer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RNFileViewer")]
+[assembly: AssemblyTitle("ReactNativeFileViewer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RNFileViewer")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("ReactNativeFileViewer")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
---
+++ windows/ReactNativeFileViewer/ReactNativeModule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.ReactNative;
+using Microsoft.ReactNative.Managed;
+using Windows.ApplicationModel.Core;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.Core;
+
+namespace ReactNativeFileViewer
+{
+    [ReactModule("RNFileViewer")]
+    internal sealed class ReactNativeModule
+    {
+        [ReactMethod("open")]
+        public async void Open(string filepath, IReactPromise<bool> promise)
+        {
+            try
+            {
+                var file = await StorageFile.GetFileFromPathAsync(filepath);
+
+                if (file != null)
+                {
+                    await CoreApplication.MainView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                    {
+                        var success = await Launcher.LaunchFileAsync(file);
+
+                        if (success)
+                        {
+                            promise.Resolve(true);
+                        }
+                        else
+                        {
+                            promise.Reject(new ReactError { Message = $"Error opening the file {filepath}" });
+                        }
+                    });
+                }
+                else
+                {
+                    promise.Reject(new ReactError { Message = $"Error loading the file {filepath}" });
+                }
+            }
+            catch (Exception e)
+            {
+                promise.Reject(new ReactError { Exception = e });
+            }
+        }
+    }
+}
